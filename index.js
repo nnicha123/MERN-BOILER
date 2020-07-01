01607662488b7788820c946c401ee423da4aba24
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-07-01 14:21:06 +0700

    up to tutorial video 14 at the moment

--- index.js
+++ index.js
@@ -5,21 +5,55 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const User = require('./model/user')
 
-mongoose.connect('mongodb+srv://nnicha:Ninat1995@firstcluster.wqh6d.mongodb.net/<dbname>?retryWrites=true&w=majority',{useNewUrlParser:true,useCreateIndex:true})
-.then(() => console.log('DB connected'))
-.catch(err => console.log(err))
+mongoose.connect('mongodb+srv://nnicha:Ninat1995@firstcluster.wqh6d.mongodb.net/<dbname>?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.log(err))
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-app.post('/api/usersnew/register',(req,res) => {
+app.get('/api/usersnew/', (req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(404).json('Error: ' + err))
+})
+
+app.post('/api/usersnew/register', (req, res) => {
     const usernew = new User(req.body)
-    usernew.save((err,userData) => {
-        if(err) return res.json({success:false,err})
-        return res.status(200).json('Completed!')
+    usernew.save((err, userData) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true, userData: userData })
     })
     return res.status(200)
 })
 
-app.listen(5000,() => console.log('Listening at port 5000'))
+app.post('/api/usersnew/login', (req, res) => {
+    // find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed,email not found"
+            })
+        }
+        // compare the password
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({loginSuccess:false,message:'wrong password'})
+            }
+        })
+        // generate token
+        user.generateToken((err,user) => {
+            if(err) return res.status(400).send(err)
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({
+                loginSuccess:true
+            })
+        })
+    })
+
+})
+
+app.listen(5000, () => console.log('Listening at port 5000'))
--- model/user.js
+++ model/user.js
@@ -1,33 +1,73 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        maxlength:50
+    name: {
+        type: String,
+        maxlength: 50
     },
-    email:{
-        type:String,
-        trim:true,
-        unique:1
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
     },
-    password:{
-        type:String,
-        minlength:5
+    password: {
+        type: String,
+        minlength: 5
     },
-    lastname:{
-        type:String,
-        maxlength:50
+    lastname: {
+        type: String,
+        maxlength: 50
     },
-    role:{
-        type:Number,
-        default:0
+    role: {
+        type: Number,
+        default: 0
     },
-    token:{
-        type:String
+    token: {
+        type: String
     },
-    tokenExp:{
-        type:Number
+    tokenExp: {
+        type: Number
     }
 })
 
-const User = mongoose.model('UserLogin',userSchema)
+// Before saving 
+userSchema.pre('save', function( next ) {
+    var user = this;
+    
+    if(user.isModified('password')){    
+        // console.log('password changed')
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+});
+
+userSchema.methods.comparePassword = (plainPassword,cb) => {
+    bcrypt.compare(plainPassword,this.password,(err,isMatch) => {
+        if(err){ return cb(err)}
+        cb(null,isMatch)
+    })
+}
+userSchema.methods.generateToken = (cb) => {
+    let user = this;
+    let token = jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token
+    user.save((err,user) => {
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+const User = mongoose.model('UserLogin', userSchema)
 module.exports = User
